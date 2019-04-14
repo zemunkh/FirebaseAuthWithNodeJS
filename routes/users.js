d764d5c9879263a5c3cf393235eafc5429514a35
zemunkh <zemunkh@gmail.com>
2019-04-14 19:53:49 +0800

    Version 9th
    Refactoring is needed.

--- routes/users.js
+++ routes/users.js
@@ -6,21 +6,25 @@ var middleware  = require("../middleware"); // it requires index.js as default
 
 var imageUploader = require("../middleware/uploader")
 
-
 var db = admin.firestore();
 
 // USER PROFILE
 router.get("/", middleware.isLoggedIn, function(req, res){
+  // Delete old files
+
   var user = firebase.auth().currentUser;
 
   var userRef = db.collection("users").doc(user.uid);
   var getDoc  = userRef.get().then(function(doc){
-    console.log("Doc before: ", doc.data());
     if(!doc.exists){
       console.log("No such document");
       return res.redirect("/secret");
     } else {
-      console.log("User retrieved data:", doc.data());
+      if (doc.data().imageURL === undefined || (doc.data().imageURL).length == 0) {
+        console.log("Yes I am empty!");
+      }
+      var time = (doc.data()).createdAt;
+      console.log("Timestamp: ", new Date(time.seconds).getTime());
       res.render("users/show", {user: doc.data()});
     }
   }).catch(function(err){
@@ -56,7 +60,7 @@ router.post("/show", function(req, res){
       t.update(userRef, data);
     });
   }).then(function(result){
-    console.log("Transaction success! Zee");
+    console.log("Transaction success! Nobo App!");
     res.redirect("/secret");
   }).catch(function(err){
     console.log("Transaction failure: ", err);
@@ -66,45 +70,41 @@ router.post("/show", function(req, res){
 
 // USER PHOTO CHANGE/UPLOAD
 router.get("/photo", middleware.isLoggedIn, function(req, res){
+  // Display cached data
   var user = firebase.auth().currentUser;
-  // Download and show user's current image from database
   var userRef = db.collection("users").doc(user.uid);
-  var getDoc  = userRef.get().then(function(doc){
+
+  // Get cached values
+  userRef.get(getOptions).then(function(doc) {
     if(!doc.exists){
       console.log("No such document");
       return res.redirect("/secret");
     } else {
       var imageURL = doc.data().imageURL;
-      console.log("imageURL: ", imageURL);
+      console.log("Cached imageURL: ", imageURL);
       res.render("users/photo", {imageURL: imageURL});
     }
-  }).catch(function(err){
-    console.log("Error getting documents **", err);
+  }).catch(function(error) {
+      console.log("Error getting cached document:", error);
+      res.redirect("/secret"); // send flash sms
   });
+
 });
 
 // photo conversion and upload for temp file
-router.post("/photo", middleware.isLoggedIn, function(req, res){
-  // Upload and reload the uploaded image
-  // Save it to user's database section
-
+router.post("/upload_photo", middleware.isLoggedIn, function(req, res){
   // Disable button in front-end when file is unavailable.
-  var image = imageUploader.uploadProfileImage(req, res);
 
-  // Upload to server
+  var filename = imageUploader.uploadProfileImage(req, res);
 
-  // get link of that uploaded images
-
-  // with user's permission, its url will set to user data.
-
-  // Update imageURL on Cloud Firestore
 });
 
-router.post("/upload", middleware.isLoggedIn, function(req, res){
+router.post("/set_profile", middleware.isLoggedIn, function(req, res){
+
   var user = firebase.auth().currentUser;
   var userRef = db.collection('users').doc(user.uid);
 
-  var imURL = req.body.imageURL;
+  var imURL = `https://storage.cloud.google.com/authdemo-f7863.appspot.com/profile/${req.body.field}`;
   // it should get the last value that user gave it to server.
   // then append to ImageURL array as first element or last element
   console.log("That URL: ", imURL);
@@ -114,7 +114,8 @@ router.post("/upload", middleware.isLoggedIn, function(req, res){
       // Array value will be added to next field value.
       "imageURL": admin.firestore.FieldValue.arrayUnion(imURL)
     }).then(function(){
-      res.redirect("/users");
+      // res.redirect("/users/photo");
+      res.send({'images': imURL});
       console.log("imageURL is successfully updated!");
     }).catch(function(err){
       console.log("Error during Update: ", err);
@@ -124,6 +125,105 @@ router.post("/upload", middleware.isLoggedIn, function(req, res){
   }
 
 });
+
+var getOptions = {
+  source: 'cache'
+};
+
+// Set Profile Images
+router.post("/photo/select", middleware.isLoggedIn, function(req, res){
+  var index        = req.body.index; //
+  var profileImage = req.body.profileImage;
+  console.log("Profile Image URL: ", profileImage);
+
+// switch it to first place of array
+// User DATA PERSISTENCE as cache method.
+
+  var user = firebase.auth().currentUser;
+  var userRef = db.collection("users").doc(user.uid);
+
+  userRef.get(getOptions).then(function(doc) {
+      console.log("Cached document data:", doc.data());
+
+      var imageArray = doc.data().imageURL;
+      if(index != 0 || index != -1){
+         var temp = imageArray[index];
+         imageArray[index] = imageArray[0];
+         imageArray[0] = temp;
+         console.log("Array is swapped! ", imageArray);
+      }
+
+      // Need to be nested
+      userRef.update({
+        // Array value will be added to next field value.
+        "imageURL": imageArray
+      }).then(function(){
+        res.render("users/photo", {imageURL: imageArray});
+        console.log("imageURL is successfully updated!");
+      }).catch(function(err){
+        console.log("Error during Update: ", err);
+      });
+      // update user data
+      // ########### Stuck in Here ##############
+
+  }).catch(function(error) {
+      console.log("Error getting cached document:", error);
+  });
+
+});
+
+function checkCache(user){
+  var userRef = db.collection("users").doc(user.uid)
+  .onSnapshot({includeMetadataChanges: true}, function(snapshot) {
+    snapshot.docChanges().forEach(function(change){
+      if(change.type === "added"){
+        console.log("File changed!");
+      }
+      var source = snapshot.metadata.fromCache ? "local cache" : "server";
+      console.log("Data came from " + source);
+    });
+  });
+}
+
+// db.collection("cities").where("state", "==", "CA")
+//   .onSnapshot({ includeMetadataChanges: true }, function(snapshot) {
+//       snapshot.docChanges().forEach(function(change) {
+//           if (change.type === "added") {
+//               console.log("New city: ", change.doc.data());
+//           }
+//
+//           var source = snapshot.metadata.fromCache ? "local cache" : "server";
+//           console.log("Data came from " + source);
+//       });
+//   });
+
+router.post("/photo/delete", middleware.isLoggedIn, function(req, res){
+  var user = firebase.auth().currentUser;
+  var userRef = db.collection('users').doc(user.uid);
+  // Maybe, I have to cache values to update array of images.
+  var selectedImage = req.body.selectedImage;
+  console.log("To be deleted image:", selectedImage);
+
+  if(selectedImage != null) {
+    userRef.update({
+      // Array value will be added to next field value.
+      "imageURL": admin.firestore.FieldValue.arrayRemove(selectedImage)
+    }).then(function(){
+      res.redirect("/users");
+      console.log("selectedImage is successfully deleted!");
+    }).catch(function(err){
+      console.log("Error during Update: ", err);
+    });
+  } else {
+    console.log("Error while retrieving imageURL!");
+  }
+
+});
+
+// for later usage
+function removeItem(items, i) {
+    return items.slice(0, i-1).concat(items.slice(i, items.length));
+}
 
 
 // Password reset
